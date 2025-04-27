111fdc59a202f3ca9688488852f494a8c311fe1b
masternetty <hugoalevi731@gmail.com>
2025-04-27 17:51:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+
 function abrirModal(id) {
     document.getElementById('modal' + id).style.display = 'flex';
 }
-
 function fecharModal(id) {
     document.getElementById('modal' + id).style.display = 'none';
 }
